509667ee32cd48e0259ba97133f7912a421bced7
선정관 <'sdfjikl8787@gmail.com'>
2023-03-08 18:30:41 +0900

    123123

--- src/main/java/com/my/spring/web/ch02/home/NumController.java
+++ src/main/java/com/my/spring/web/ch02/home/NumController.java
@@ -1,5 +1,30 @@
 package com.my.spring.web.ch02.home;
 
-public class NumController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller("ch02.home")
+@RequestMapping("ch02/home")
+public class NumController {
+	@GetMapping("numIn") //@Nullable도 가능하긴함
+	public String numIn() {
+		return "ch02/home/numIn";
+	}
+	
+	@PostMapping("numOut")
+	public String numOut(@ModelAttribute("num")int num) {
+		
+		return "redirect:numIn";
+	}
 }
+/*
+폼에 자연수 하나를 입력한다. 
+폼을 제출한다.
+입력값 <= x < 입력값 + 1인, x 를 출력한다.
+x는 실수이다.
+x는 랜덤값이다.
+x는 소수점 이하 한자리까지 표현한다.
+*/
