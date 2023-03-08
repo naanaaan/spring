e363aaf3931a3883fea9f12c22ea5cdfe681b679
선정관 <'sdfjikl8787@gmail.com'>
2023-03-08 15:34:19 +0900

    ㅇㅇ

--- src/main/java/com/my/spring/web/ch02/ex01/User.java
+++ src/main/java/com/my/spring/web/ch02/ex01/User.java
@@ -3,7 +3,7 @@ package com.my.spring.web.ch02.ex01;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//Data쓰면 lombok에서 getter setter만ㄷ르어줌
+
 @Data
 @NoArgsConstructor //생성자
 @AllArgsConstructor
